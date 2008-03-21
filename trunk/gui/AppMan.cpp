eb12e075495f69588fc63d8ae61802c1da56937a
ickard <ickard@5cb44641-4943-0410-9335-857100fbb57d>
2008-03-21 16:40:37 +0000

    added some socket watching stuff that might work

--- trunk/gui/AppMan.cpp
+++ trunk/gui/AppMan.cpp
@@ -1,15 +1,29 @@
 #include "AppMan.h"
 
+void AppMan::setupSocket()
+{
+	int fd = m_handler->getSocket();
+
+	Glib::signal_io().connect(
+		sigc::mem_fun(*this,&AppMan::on_socket_changed),fd,Glib::IO_IN);
+}
+
+bool AppMan::on_socket_changed(Glib::IOCondition io)
+{
+	m_handler->update();
+
+	return true;
+}
+
 
 AppMan::AppMan()
 {
 	m_handler = new Handler();
-
 	m_window = new MainWindow(m_handler);
-
 	m_prefswindow = new PrefsWindow(m_window);
 	m_icon = new TrayIcon(m_window,m_prefswindow);
 
+	setupSocket();	
 	m_prefswindow->loadSettings();
 
 	m_window->show();
