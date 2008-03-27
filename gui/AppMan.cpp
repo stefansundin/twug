59f0ee76363c74a6587f8b3feaf9d9ef5d61baa3
Sebastian Ärleryd <baskus@users.noreply.github.com>
2008-03-27 22:57:29 +0000

    crap

--- gui/AppMan.cpp
+++ gui/AppMan.cpp
@@ -2,15 +2,26 @@
 
 void AppMan::setupSocket()
 {
-	int fd = m_handler->getSocket();
+//	int fd = m_handler->getSocket();
+
+	int fd = open("kaka.txt", O_RDONLY);
 
 	Glib::signal_io().connect(
 		sigc::mem_fun(*this,&AppMan::on_socket_changed),fd,Glib::IO_IN);
+	Glib::IOChannel::create_from_fd(fd);
 }
 
 bool AppMan::on_socket_changed(Glib::IOCondition io)
 {
-	m_handler->update();
+	if((io & Glib::IO_IN) == 0)
+	{
+		printf("no good\n");
+	}
+	else
+	{
+//		printf("good\n");
+		m_handler->update();
+	}
 
 	return true;
 }
